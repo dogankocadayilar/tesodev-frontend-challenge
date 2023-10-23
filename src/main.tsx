3aa953c0435e8ca408a39c3c33d29a0f2c70b1d5
dogankocadayilar <dogankocadayilar@gmail.com>
2023-10-23 20:30:45 +0300

    Fix: BrowserRouter basename

--- src/main.tsx
+++ src/main.tsx
@@ -8,9 +8,9 @@ import Records from "./pages/Records";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/tesodev-frontend-challenge">
       <Routes>
-        <Route path="/tesodev-frontend-challenge">
+        <Route path="/">
           <Route index element={<Home />} />
           <Route path="records" element={<Records />} />
           <Route path="records/new" element={<NewRecord />} />
